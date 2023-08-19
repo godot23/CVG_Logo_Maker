784fdb22645ece7c0ede3e6aaaae0b1f8eb48e04
godot23 <jasonanavon@gmail.com>
2023-08-19 15:04:55 -0400

    got shapes to write to file and take user input

--- index.js
+++ index.js
@@ -1,9 +1,59 @@
 const fs = require("fs");
-let SVG = require("./lib/svg")
+let SVG = require("./lib/svg");
+const inquirer = require("inquirer");
+const shape = require('./lib/shapes.js');
 
-let example = new SVG("AND", "green", "triangle", "yellow")
+// let example = new SVG("AND", "green", "triangle", "yellow")
 
-fs.writeFile("shape.svg", example.markUp, function(err){
-    if(err)
-    console.log(err)
-})
+inquirer
+    .prompt([
+        {
+            type: "list",
+            name: "shape",
+            message: "please choose a shape",
+            choices: [
+                'square',
+                'circle',
+                'triangle',
+            ]
+        },
+        {
+            type: 'input',
+            name: 'color',
+            message: 'please input a color for the shape',
+        },
+        {
+            type: 'input',
+            name: 'letters',
+            message: 'please input 3 letters for display in your shape'
+        },
+        {
+            type: "input",
+            name: "letterColor",
+            message: "please input a color for the text"
+        }
+    ])
+
+    .then((answers) =>{
+        if(answers.shape === "square"){
+            return new shape.Square(answers.color, answers.letters, answers.letterColor);
+
+        }
+        if(answers.shape === "circle"){
+            return new shape.Circle(answers.color, answers.letters, answers.letterColor);
+           
+        }
+        if(answers.shape === "triangle"){
+            return new shape.Triangle(answers.color, answers.letters, answers.letterColor);
+        }
+
+    })
+
+    .then((shape) => {
+        fs.writeFile("shape.svg", shape.render(), function(err){
+            if(err)
+            console.log(err)
+        })
+    })
+    
+
